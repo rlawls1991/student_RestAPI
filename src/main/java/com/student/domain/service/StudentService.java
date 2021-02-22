a48cf921e42ade3530bad0883c412a87606b638b
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-02-22 11:03:40 +0900

    feat - StudentService roll back

---
+++ src/main/java/com/student/domain/service/StudentService.java
@@ -0,0 +1,55 @@
+package com.student.domain.service;
+
+
+import com.student.domain.Student;
+import com.student.domain.dto.SearchDto;
+import com.student.domain.dto.StudentDto;
+import com.student.domain.dto.StudentInputDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface StudentService {
+
+    /**
+     * 학생을 생성하는 서비스
+     *
+     * @return
+     */
+    public StudentDto createStudent(final StudentInputDto studentInput);
+
+
+    /**
+     * 학생을 조회하는 서비스
+     * @param studentInput
+     * @return
+     */
+    public StudentDto searchStudent(final StudentInputDto studentInput);
+
+    /**
+     * 학생들을 조회하는 서비스
+     *
+     * @return
+     */
+    public Page<StudentDto> queryStudent(final Pageable pageable, final SearchDto searchDto);
+
+    /**
+     * 학생 한명을 조회하는 서비스
+     *
+     * @return
+     */
+    public StudentDto getStudent(final Long id);
+
+    /**
+     * 학생 정보를 수정하는 서비스
+     *
+     * @return
+     */
+    public StudentDto updateStudent(final Long id, final StudentInputDto studentInput);
+
+    /**
+     * 학생을 삭제하는 서비스(Y->N으로 수정)
+     *
+     * @return
+     */
+    public void deleteStudent(final Long id);
+}
---
+++ src/main/java/com/student/domain/service/StudentServiceImpl.java
@@ -0,0 +1,73 @@
+package com.student.domain.service;
+
+import com.student.domain.Student;
+import com.student.domain.dto.SearchDto;
+import com.student.domain.dto.StudentDto;
+import com.student.domain.dto.StudentInputDto;
+import com.student.domain.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    @Transactional
+    public StudentDto createStudent(final StudentInputDto studentInput) {
+        Student student = modelMapper.map(studentInput, Student.class);
+        student.createSetting();
+        Student saveStudent = studentRepository.save(student);
+
+        return modelMapper.map(saveStudent, StudentDto.class);
+    }
+
+    @Override
+    public StudentDto searchStudent(final StudentInputDto studentInput) {
+        return studentRepository.findByStudent(studentInput);
+    }
+
+    @Override
+    public Page<StudentDto> queryStudent(final Pageable pageable, SearchDto searchDto) {
+        return studentRepository.findAll(searchDto, pageable);
+    }
+
+    @Override
+    public StudentDto getStudent(final Long id) {
+        StudentDto student = studentRepository.findByStudent(id);
+
+        if (student == null) {
+            return null;
+        }
+
+        return student;
+    }
+
+    @Override
+    @Transactional
+    public StudentDto updateStudent(final Long id, final StudentInputDto studentInput) {
+        StudentDto studentDto = studentRepository.findByStudent(id);
+
+        studentDto.setAge(studentInput.getAge());
+        studentDto.setName(studentInput.getName());
+        studentDto.setAddress(studentInput.getAddress());
+        studentDto.setEmail(studentInput.getEmail());
+        studentDto.setPhone(studentInput.getPhone());
+
+        return studentDto;
+    }
+
+    @Override
+    @Transactional
+    public void deleteStudent(final Long id) {
+        studentRepository.deleteById(id);
+    }
+}
